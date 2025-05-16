303862b0a175261387221830cff5b3a70f65cc71
harshad8180 <hpborkar@gmail.com>
2025-05-16 16:32:38 +0530

    rearrange and applying theme to manage it

---
+++ src/Pages/HomePage.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+import About from "../components/About";
+import Values from "../components/Values";
+import Facts from "../components/Facts";
+import Features from "../components/Features";
+
+export default function HomePage() {
+  return (
+    <>
+      <section id="hero" className="hero section">
+        <div className="container">
+          <div className="row gy-4">
+            <div className="col-lg-6 order-2 order-lg-1 d-flex flex-column justify-content-center">
+              <h1 data-aos="fade-up">
+                We offer modern solutions for growing your business
+              </h1>
+              <p data-aos="fade-up" data-aos-delay="100">
+                We are team of talented designers making websites with Bootstrap
+              </p>
+              <div
+                className="d-flex flex-column flex-md-row"
+                data-aos="fade-up"
+                data-aos-delay="200"
+              >
+                <a href="#about" className="btn-get-started">
+                  Get Started <i className="bi bi-arrow-right"></i>
+                </a>
+                <a
+                  href="https://www.youtube.com/watch?v=Y7f98aduVJ8"
+                  className="glightbox btn-watch-video d-flex align-items-center justify-content-center ms-0 ms-md-4 mt-4 mt-md-0"
+                >
+                  <i className="bi bi-play-circle"></i>
+                  <span>Watch Video</span>
+                </a>
+              </div>
+            </div>
+            <div
+              className="col-lg-6 order-1 order-lg-2 hero-img"
+              data-aos="zoom-out"
+            >
+              <img
+                src="assets/img/hero-img.png"
+                className="img-fluid animated"
+                alt=""
+              />
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <About />
+      <Values />
+      <Facts />
+      <Features />
+    </>
+  );
+}
---
+++ src/components/Features.jsx
@@ -0,0 +1,69 @@
+import React from "react";
+
+export default function Features() {
+  return (
+    <section id="features" className="features section">
+      <div className="container section-title" data-aos="fade-up">
+        <h2>Features</h2>
+        <p>
+          Our Advacedd Features
+          <br />
+        </p>
+      </div>
+
+      <div className="container">
+        <div className="row gy-5">
+          <div className="col-xl-6" data-aos="zoom-out" data-aos-delay="100">
+            <img src="assets/img/features.png" className="img-fluid" alt="" />
+          </div>
+
+          <div className="col-xl-6 d-flex">
+            <div className="row align-self-center gy-4">
+              <div className="col-md-6" data-aos="fade-up" data-aos-delay="200">
+                <div className="feature-box d-flex align-items-center">
+                  <i className="bi bi-check"></i>
+                  <h3>Eos aspernatur rem</h3>
+                </div>
+              </div>
+
+              <div className="col-md-6" data-aos="fade-up" data-aos-delay="300">
+                <div className="feature-box d-flex align-items-center">
+                  <i className="bi bi-check"></i>
+                  <h3>Facilis neque ipsa</h3>
+                </div>
+              </div>
+
+              <div className="col-md-6" data-aos="fade-up" data-aos-delay="400">
+                <div className="feature-box d-flex align-items-center">
+                  <i className="bi bi-check"></i>
+                  <h3>Volup amet volupt</h3>
+                </div>
+              </div>
+
+              <div className="col-md-6" data-aos="fade-up" data-aos-delay="500">
+                <div className="feature-box d-flex align-items-center">
+                  <i className="bi bi-check"></i>
+                  <h3>Rerum omnis sint</h3>
+                </div>
+              </div>
+
+              <div className="col-md-6" data-aos="fade-up" data-aos-delay="600">
+                <div className="feature-box d-flex align-items-center">
+                  <i className="bi bi-check"></i>
+                  <h3>Alias possimus</h3>
+                </div>
+              </div>
+
+              <div className="col-md-6" data-aos="fade-up" data-aos-delay="700">
+                <div className="feature-box d-flex align-items-center">
+                  <i className="bi bi-check"></i>
+                  <h3>Repellendus molli</h3>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
